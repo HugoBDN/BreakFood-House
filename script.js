b58a98180d12f5398ee5513dab296b8b7dcb3a13
HugoBDN <hugo.bedouin@gmail.com>
2023-12-01 09:17:03 +0100

    version quasi fini

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const openBtn = document.getElementById("openBtn");
 const sidenav = document.getElementById("mySidenav");
 const closeBtn = document.getElementById("closeBtn");
-const arrow = document.querySelector(".arrow")
-const onglets = document.getElementById(".onglets")
+const arrow = document.getElementById("arrow")
+const onglets = document.getElementById("onglets")
 
 
 
@@ -13,12 +13,14 @@ arrow.onclick = openOnglets;
 
 
 function openOnglets() {
+    if (!onglets.classList.contains("active")) {
     onglets.classList.add("active");
-    if(arrow.onclick=true) {
-        onglets.classList.remove("active");
-    }
+    document.body.classList.add("active");
+} else {
+    onglets.classList.remove("active");
+    document.body.classList.remove("active");
+  }
 }
-
 
 function openNav() {
     sidenav.classList.add("active");
